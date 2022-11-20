1d65933093758af6a2f9f2f3f7337ef702708502
Diana <maritimdiane96@gmail.com>
2022-11-20 14:49:03 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,60 @@
-// Write your solution in this file!
+/*/ Write your solution in this file!
+describe('employees', function() {
+    describe('updateEmployeeWithKeyAndValue(employee, key, value)', function () {
+      beforeEach(function () {
+        for (const key in employee) {
+          delete employee[key];
+        }
+
+        employee.name = 'Sam';
+      });
+function employees{
+function updateEmployeeWithKeyAndValue(employee, key, value)
+function beforeEach */
+
+
+let employee = {
+
+  name: 'sam',
+  streetAddress: '11 Broadway',
+
+  }
+
+
+  function updateEmployeeWithKeyAndValue(employee, key, value) {
+
+  let employees = {...employee}
+
+  employees[key] = value
+
+  return employees;
+
+  }
+
+
+
+  function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+
+  employee[key] = value
+
+  return employee
+
+  }
+
+  function deleteFromEmployeeByKey (employee, key) {
+
+  let newEmployee = {employee}
+
+  delete newEmployee[key]
+
+  return newEmployee
+
+  }
+
+  function destructivelyDeleteFromEmployeeByKey(employee,key) {
+
+  delete employee[key]
+
+  return employee
+
+  }
